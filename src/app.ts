48b0fc8e63bcada3cedc51c53f740cad5eabc2cd
JellinekGy <90341733+JellinekGy@users.noreply.github.com>
2022-01-08 14:17:10 +0100

    Homework done

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 // Importáld be a HumanHero és TransformerHero osztályokat.
-
+import { HumanHero } from './hero';
+import { TransformerHero } from './transformer';
 
 /**
  * Állítsd be helyesen a változót!
@@ -8,7 +9,7 @@
  * Hozz létre benne 3 HumanHero példányt, szabadon választott adatokkal.
  * @var {HumanHero[]} humans
  */
-export const humans: any[] = [];
+export const humans: any[] = [new HumanHero(1, "Mikaela Banes", "female", 20, 100), new HumanHero(2, "Sam WitWicky", "male", 20, 100), new HumanHero(3, "Charlie Watson", "female", 29, 100)];
 
 /**
  * Állítsd be helyesen a változót!
@@ -17,4 +18,4 @@ export const humans: any[] = [];
  * Hozz létre benne 3 TransformerHero példányt, szabadon választott adatokkal.
  * @var {TransformerHero[]} transformers
  */
-export const transformers: any[] = [];
+export const transformers: any[] = [new TransformerHero(1, "Optimus Prime", 0, 6, "Autobot"), new TransformerHero(2, "Bumblebee", 0, 4, "Autobot"), new TransformerHero(3, "Starscream", 2, 0, "Decepticon")];
